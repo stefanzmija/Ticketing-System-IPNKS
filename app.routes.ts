b21d1041181115384fd7f5961a2ee95b8a1116d7
stefanzmija <stefannaumovski2005@gmail.com>
2026-09-18 17:32:58 +0200

    Add navbar

---
+++ app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import {Home} from './features/home/home';
+
+export const routes: Routes = [
+  {path: "home", component: Home},
+  {path: "**", redirectTo: '/home'},
+];
---
+++ features/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Navbar} from '../navbar/navbar';
+
+@Component({
+  imports: [
+    Navbar
+  ],
+  selector: 'app-home',
+  styleUrl: './home.css',
+  templateUrl: './home.html',
+})
+export class Home {}
